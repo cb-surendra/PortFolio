89ca11e055a259636936e59c5bee603d46098d6e
cb-surendra <surendra.p@chargebee.com>
2019-12-23 15:24:16 +0530

    added the portfolio first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <SideBar/>
+  </div>
+</template>
+
+<script>
+import SideBar from './SideBar';
+
+export default {
+  name: 'home',
+  components: {
+    SideBar
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,58 @@
+<template>
+    <div >
+        <div class="sidebar-content">
+            <div class="dp">
+                <img src='../assets/profile.png'>
+            </div>
+            <a href="#about">ABOUT</a>
+            <a href="#"> <strong> EXPERIENCE </strong> </a>
+            <a href="#"> <strong> EDUCATION </strong> </a>
+            <a href="#"> <strong> SKILLS </strong> </a>
+            <a href="#"> <strong> INTEREST </strong> </a>
+            <a href="#"> <strong> CONTACTS </strong> </a>
+        </div>
+    </div>
+</template>
+ 
+<script>
+
+/* eslint-disable */
+
+export default {
+  name: 'Index',
+  data(){
+    return {
+        profile
+    }
+  }
+};
+</script>
+<style scoped>
+.sidebar-content{
+    background-color: #040707d7;
+    height: 100%;
+    text-align: center;
+    position: absolute;
+    padding-top: 15%;
+    padding-left:25%;
+    padding-right: 25%;
+}
+.sidebar-content a{
+    text-decoration: none;
+    padding: 5%;
+    display: block;
+    color: #f0f0e6d7;
+    font-size: 20px;
+    /* transform: translateY(-50%); */
+}
+.sidebar-content a:hover{
+    color: #ff8a80;
+}
+img{
+   height: 200px;
+   width: 200px;
+   background-color: white;
+   border-radius: 50%;
+   margin-bottom: 25%;
+}
+</style>
